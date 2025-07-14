a62cffb5fcf1e2bcef7fce31a68cfca11e7c1f27
Aniket <aniketpatel6264@gmail.com>
2025-07-15 00:48:45 +0530

    Billing service initial setup commit

---
+++ biiling-service/src/main/java/com/pm/biilingservice/BiilingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.biilingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiilingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiilingServiceApplication.class, args);
+    }
+
+}
---
+++ biiling-service/src/test/java/com/pm/biilingservice/BiilingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.biilingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiilingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
