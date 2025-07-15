02a0c30797341371dbd01778a445103aa8dc9b41
Aniket <aniketpatel6264@gmail.com>
2025-07-15 21:00:59 +0530

    refactor

--- billing-service/src/main/java/com/pm/biilingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/pm/biilingservice/BillingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BiilingServiceApplication {
+public class BillingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BiilingServiceApplication.class, args);
+        SpringApplication.run(BillingServiceApplication.class, args);
     }
 
 }
