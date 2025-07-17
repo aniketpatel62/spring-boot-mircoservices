a8606e4b37ac0639e428605ed6388ad51dbe6fe9
Aniket <aniketpatel6264@gmail.com>
2025-07-17 23:09:00 +0530

    DTOs

--- auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
@@ -3,7 +3,9 @@ package com.pm.authservice.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class LoginRequestDTO {
     @NotBlank(message = "Email is required")
     @Email(message = "Provide a valid email address")
