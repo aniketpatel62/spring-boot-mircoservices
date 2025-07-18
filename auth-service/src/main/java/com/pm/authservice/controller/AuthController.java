1a585fe4c2f2ebdad05d55a1d40a8d6b2c143a9d
Aniket <aniketpatel6264@gmail.com>
2025-07-18 23:58:07 +0530

    validateToken endpoint and api-request

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -3,13 +3,10 @@ package com.pm.authservice.controller;
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
 import com.pm.authservice.service.AuthService;
-import com.pm.authservice.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -35,5 +32,22 @@ public class AuthController {
 
         String token = tokenOptional.get();
         return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @Operation(summary = "Validate token")
+    @GetMapping("/validate")
+
+    // fetches the Authorization token from the request header. Standard to send JWT tokens in APIs.
+    // not passing token directly in query string: unsafe
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+
+        // Authorization : Bearer <token> (7th char)
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
